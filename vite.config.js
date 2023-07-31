c6f7bee8f47752441be50fc5bc0e389534d8f79c
KMagdi <46417302+kmagdi@users.noreply.github.com>
2023-07-31 09:38:10 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   
-    plugins: [react()]
+    plugins: [react()],
+    build: {
+        chunkSizeWarningLimit: 1600
+    }
+   
 });
